34e48aee51cef2a1ca589d276a4c9ca1971033ce
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-07-06 22:26:35 +0900

    네비게이션 그래프 추가

--- app/src/main/java/com/minjaee/restareaapp/MainActivity.kt
+++ app/src/main/java/com/minjaee/restareaapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.minjaee.restareaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import com.minjaee.restareaapp.databinding.ActivityMainBinding
+import com.minjaee.restareaapp.presentation.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+//    lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+//        viewModel = ViewModelProvider(this, )
     }
 }
